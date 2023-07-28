4a75bd303bc0b9cfce59004abe41d1243c377c59
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-07-28 00:20:57 -0500

    Added contact form.

--- mtc-portfolio-react/src/components/Contact.jsx
+++ mtc-portfolio-react/src/components/Contact.jsx
@@ -2,9 +2,29 @@ function Contact() {
     return (
       <section>
         <h3>Contact</h3>
-        <div id="phone">312-888-6917</div>
-        <div id="email"><a href="mailto:cunha.maria.theresa@gmail.com">cunha.maria.theresa@gmail.com</a></div>
-        <div id="git-hub"><a href="https://github.com/MCunha17">GitHub</a></div>
+        <form className="form-horizontal" action="/action_page.php">
+          <div className="form-group">
+            <label className="control-label col-sm-2" htmlFor="name">Name:</label>
+            <div className="col-sm-10">
+              <input type="name" className="form-control" id="name" placeholder="Enter name" />
+            </div>
+          </div>
+          <div className="form-group">
+            <label className="control-label col-sm-2" htmlFor="email">Email Address:</label>
+            <div className="col-sm-10">
+              <input type="email" className="form-control" id="email" placeholder="Enter email address" />
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="Message">Message:</label>
+                <textarea class="form-control" rows="5" id="message"></textarea>
+            </div>
+          <div className="form-group">
+            <div className="col-sm-offset-2 col-sm-10">
+              <button type="submit" className="btn btn-default">Submit</button>
+            </div>
+          </div>
+        </form>
       </section>
     );
   }
