ff29226bb2828f221eb4d0679250a6113fce151f
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-07-27 23:08:38 -0500

    Added Footer component.

---
+++ mtc-portfolio-react/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer>
+        {/* placeholder for footer content */}
+        <h2>Footer</h2>
+      </footer>
+    );
+  }
+  
+  export default Footer;  
