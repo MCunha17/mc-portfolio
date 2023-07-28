f48d5364c7004a617fba771fcd73f70f9dac7eca
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-07-27 23:06:33 -0500

    Added AboutMe component.

---
+++ mtc-portfolio-react/src/components/AboutMe.jsx
@@ -0,0 +1,10 @@
+function AboutMe() {
+    return (
+      <div>
+        {/* placeholder for 'About Me' content */}
+        <h2>About Me</h2>
+      </div>
+    );
+  }
+  
+  export default AboutMe;  
