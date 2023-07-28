30e2bb687a76aab4717e84de79ad33014a6d255f
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-07-27 23:10:23 -0500

    Updated App.jsx to include components.

--- mtc-portfolio-react/src/App.jsx
+++ mtc-portfolio-react/src/App.jsx
@@ -1,5 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
 
 function App() {
   const [selectedSection, setSelectedSection] = useState('About Me');
