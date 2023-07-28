bb9ba55f702daf5e20b0d5af77356ad49bbe1e20
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-07-28 12:25:45 -0500

    Added social icons to footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,28 @@
+import React from 'react';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+
 function Footer() {
-    return (
-      <footer>
-        <h5>Created by Maria Cunha, 2023.</h5>
-      </footer>
-    );
-  }
-  
-export default Footer;  
+  return (
+    <footer>
+      <h5>Created by Maria Cunha, 2023.</h5>
+      <div className="social-links">
+        <a
+          href="https://www.linkedin.com/in/mariacunha"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin size={24} />
+        </a>
+        <a
+          href="https://github.com/MCunha17"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub size={24} />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
