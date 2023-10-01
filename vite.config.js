13b771f078b0fdec1741676eb3603e6d225e00c6
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-10-01 15:35:17 -0500

    Reverted back to working app code.

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,11 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './src/main.jsx',
+        index: './index.html'
+      }
+    }
   },
 });
